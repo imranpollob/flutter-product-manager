a32520df6c24c709ddef31a132dfedb52a34e94e
pollmix <polboy777@gmail.com>
2019-03-30 00:20:16 +0600

    Add card by clicking button

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import './product_manager.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('At last!! working'),
+        ),
+        body: ProductManager('Basic Food Tester'),
+      ),
+    );
+  }
+}
